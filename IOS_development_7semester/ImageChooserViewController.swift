678208b09151834b9a25d20004cbe66f6d04de95
NIOHOMY <NIOHOMY@mail.ru>
2024-10-08 10:40:26 -0700

    lab-3 - Добавлено 3 картинки на выбор для загрузки и segue для каждой

--- IOS_development_7semester/ImageChooserViewController.swift
+++ IOS_development_7semester/ImageChooserViewController.swift
@@ -7,21 +7,36 @@ class ImageChooserViewController: UIViewController {
 
     let imageURLs: [String: URL] = [
         "image1": URL(string: "https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340")!,
-        "image2": URL(string: "https://example.com/image2.jpg")!
+        "image2": URL(string: "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg")!,
+        "image3": URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCy16nhIbV3pI1qLYHMJKwbH2458oiC9EmA&s")!
     ]
 
-    @IBAction func ChooseBtnTapped(_ sender: Any) {
-        let selectedIdentifier = "image1"
-        selectedImageURL = imageURLs[selectedIdentifier]
-        
-        performSegue(withIdentifier: "unwindToImageViewController", sender: self)
+    @IBAction func ChooseBtnOneTapped(_ sender: Any) {
+        performSegue(withIdentifier: "image1", sender: self)
+    }
+    
+    @IBAction func ChooseBtnTwoTapped(_ sender: Any) {
+        performSegue(withIdentifier: "image2", sender: self)
+    }
+    
+    @IBAction func ChooseBtnTreeTapped(_ sender: Any) {
+        performSegue(withIdentifier: "image3", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("prepare")
-        if segue.identifier == "unwindToImageViewController", let vc =
+        if segue.identifier == "image1", let vc =
             segue.destination as? ImageViewController {
                 let selectedIdentifier = "image1"
+                selectedImageURL = imageURLs[selectedIdentifier]
+                vc.imageURL = selectedImageURL
+        } else if segue.identifier == "image2", let vc =
+            segue.destination as? ImageViewController {
+                let selectedIdentifier = "image2"
+                selectedImageURL = imageURLs[selectedIdentifier]
+                vc.imageURL = selectedImageURL
+        } else if segue.identifier == "image3", let vc =
+            segue.destination as? ImageViewController {
+                let selectedIdentifier = "image3"
                 selectedImageURL = imageURLs[selectedIdentifier]
                 vc.imageURL = selectedImageURL
         }
